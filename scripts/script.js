fcd7aa95c6712f231b453c08c3935a39d917cfcd
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-04-20 14:44:51 +0800

    added light dark mode

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const toggle = document.getElementById("toggle");
+const refresh = document.getElementById("refresh");
+const theme = window.localStorage.getItem("theme");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+if (theme === "dark") document.body.classList.add("dark");
+
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (theme === "dark") {
+      window.localStorage.setItem("theme", "light");
+    } else window.localStorage.setItem("theme", "dark");
+  });
+
+refresh.addEventListener("click", () => {
+  window.location.reload();
+});
+
+
+
+
+
+
+
+
